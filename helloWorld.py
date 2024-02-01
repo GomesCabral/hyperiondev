dce6d313bbb59db6d7f918e0f504ccb6aab49574
Gomes Cabral <phgcabral@hotmail.com>
2024-02-01 15:49:54 +0100

    modifief the file helloWorld.py to ask for age

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,4 @@
-user_input = input("Insert your name: ")
-print(f"Hello {user_input}")
+user_name = input("Insert your name: ")
+user_age = input("Insert your age: ")
+
+print(f"Hello {user_name}!\nYou have {user_age} years.")
